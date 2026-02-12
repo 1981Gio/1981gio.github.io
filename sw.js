7990398e4a4957ae69bd0eb05c605fa2a5b3227f
1981Gio <giovannicalise81@gmail.com>
2026-02-12 11:08:43 +0100

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
-const CACHE_NAME = "giorders-cache-v1";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./1FFB8DDC-1EC1-4569-971E-598213CC5587.png"
-      ]);
-    })
-  );
+self.addEventListener("install",e=>{
+self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch",()=>{});
