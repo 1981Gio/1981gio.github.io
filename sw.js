ccd5ca211057236086ff462742e3d8d320a23b67
1981Gio <giovannicalise81@gmail.com>
2026-02-12 10:44:33 +0100

    Update service worker cache name and caching logic

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
-const CACHE_NAME = "giorders-pro-v1";
+const CACHE_NAME = "giorders-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./1FFB8DDC-1EC1-4569-971E-598213CC5587.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
