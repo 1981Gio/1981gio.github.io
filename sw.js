d89f65e2bbd51f7d09c9f8bdf7ad38f265d57656
1981Gio <giovannicalise81@gmail.com>
2026-02-12 10:59:34 +0100

    Rename cache and update caching logic

--- sw.js
+++ sw.js
@@ -1,24 +1,22 @@
-const CACHE_NAME = "giorders-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./1FFB8DDC-1EC1-4569-971E-598213CC5587.png"
-];
+const CACHE_NAME = "giorders-cache-v1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./1FFB8DDC-1EC1-4569-971E-598213CC5587.png"
+      ]);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
